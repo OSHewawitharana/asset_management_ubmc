2fa10fcc5d3e70533543470662b688543276287a
OSHewawitharana <oshewawitharana123@gmail.com>
2023-06-05 11:21:57 +1000

    initial commits

---
+++ UBMC/AddAssetType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UBMC
+{
+    public partial class AddAssetType : Form
+    {
+        public AddAssetType()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ UBMC/Login.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace UBMC
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginTitle = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.inputUsername = new System.Windows.Forms.TextBox();
+            this.inputPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginTitle
+            // 
+            this.loginTitle.AutoSize = true;
+            this.loginTitle.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginTitle.Location = new System.Drawing.Point(299, 69);
+            this.loginTitle.Name = "loginTitle";
+            this.loginTitle.Size = new System.Drawing.Size(116, 45);
+            this.loginTitle.TabIndex = 0;
+            this.loginTitle.Text = "Login";
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(176, 170);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(73, 17);
+            this.username.TabIndex = 1;
+            this.username.Text = "Username";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(176, 208);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(69, 17);
+            this.password.TabIndex = 2;
+            this.password.Text = "Password";
+            // 
+            // inputUsername
+            // 
+            this.inputUsername.Location = new System.Drawing.Point(295, 170);
+            this.inputUsername.Name = "inputUsername";
+            this.inputUsername.Size = new System.Drawing.Size(259, 22);
+            this.inputUsername.TabIndex = 3;
+            // 
+            // inputPassword
+            // 
+            this.inputPassword.Location = new System.Drawing.Point(295, 208);
+            this.inputPassword.Name = "inputPassword";
+            this.inputPassword.Size = new System.Drawing.Size(259, 22);
+            this.inputPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(316, 296);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 29);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.inputPassword);
+            this.Controls.Add(this.inputUsername);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.loginTitle);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginTitle;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox inputUsername;
+        private System.Windows.Forms.TextBox inputPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
